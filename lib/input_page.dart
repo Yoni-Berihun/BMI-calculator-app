95ec7bca421d4e2cedee1507e0d2a92b593889a3
Yoni-Berihun <yonatanberihun1998@gmail.com>
2025-09-03 13:07:20 +0300

    finished initial commit for my Cool BMI caluclator APP Gracefully

---
+++ lib/input_page.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Column(
+        children: [
+          // First Row — Two Cards
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(
+                    colour:Color(0xFF1D1E33),
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    colour:Color(0xFF1D1E33),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          // Second Row — One Full-Width Card
+          Expanded(
+            child: ReusableCard(
+                    colour:Color(0xFF1D1E33),
+                  ),
+          ),
+
+          // Third Row — Two Cards
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(
+                    colour:Color(0xFF1D1E33),
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    colour:Color(0xFF1D1E33),
+                  )
+                ),
+              ],
+            ),
+          ),_
+        ],
+      ),
+
+     
+    );
+  }
+}
+ class ReusableCard extends StatelessWidget
+ {
+   final Color colour;
+  ReusableCard({required this.colour });
+   @override 
+   Widget build(BuildContext context)
+   {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration:BoxDecoration(
+        color:colour,
+        borderRadius:BorderRadius.circular(10.0),
+
+      ),
+    );
+   }
+ 
+ }
+      
